3f8def4c4237e936bfe3fae4f12b63d590f5a691
zgbsss <gabrielasaraiva.04@gmail.com>
2023-06-13 13:22:26 -0300

    Ligação com a página principal

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,10 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { MaterialIcons } from "@expo/vector-icons";
 
-import Home from '.screens/Home';
-import Busca from '.screens/Busca';
-import Perfil from '.screens/Perfil';
-import Pedidos from '.screens/Pedidos';
+import Home from './screens/Home';
+import Busca from './screens/Busca';
+import Perfil from './screens/Perfil';
+import Pedidos from './screens/Pedidos';
 
 const BottomTab = createBottomTabNavigator();
 
@@ -21,7 +21,7 @@ export default function Routes() {
                     options={{
                         tabBarLabel: 'Home',
                         tabBarIcon: ({color}) => (
-                            <MaterialIcons name="home" color={color} size={26}/>
+                            <MaterialIcons name="home" color={color} size={26} />
                         ),
                     }}
                 />
@@ -31,17 +31,29 @@ export default function Routes() {
                     options={{
                         tabBarLabel: 'Busca',
                         tabBarIcon: ({color}) => (
-                            <MaterialIcons name="search" color={color} size={26}/>
+                            <MaterialIcons name="search" color={color} size={26} />
                         )
                     }}
                 />
                 <BottomTab.Screen
-                    name="Perfil"
-                    component={Perfil}
-                />
-                <BottomTab.Screen
                     name="Pedidos"
                     component={Pedidos}
+                    options={{
+                        tabBarLabel: 'Pedidos',
+                        tabBarIcon: ({color}) => (
+                            <MaterialIcons name="assignment" color={color} size={26}  />
+                        )
+                    }}
+                />
+                                <BottomTab.Screen
+                    name="Perfil"
+                    component={Perfil}
+                    options={{
+                        tabBarLabel: 'Perfil',
+                        tabBarIcon: ({color}) => (
+                            <MaterialIcons name="person" color={color} size={26} />
+                        )
+                    }}
                 />
             </BottomTab.Navigator>
         </NavigationContainer>
